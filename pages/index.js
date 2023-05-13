37caac139e2d4269cff8eac3191f4c1e56b2ef82
Shahzaib-ur-Rehman <shahzaib@hq.crunchcraft.com>
2023-05-14 00:49:47 +0500

    home page bug fixed

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Banner from "../components/Banner/Banner";
+import GetStarted from "../components/GetStarted/GetStarted";
 import InstantWithDraws from "../components/InstantWithDraws/InstantWithDraws";
 import Numbers from "../components/Numbers/Numbers";
 import TradingStratigy from "../components/TradingStrategy/TradingStratigy";
@@ -10,20 +11,7 @@ export default function Home() {
       <InstantWithDraws />
       <TradingStratigy />
       <Numbers/>
-      <section className="getStarted text-center">
-        <div className="container">
-          <h2>Ready to get started?</h2>
-          <p>
-            It only takes 3 minutes to get your account set up <br />
-            and ready for trading
-          </p>
-          <br />
-          <br />
-          <a href="#" className="btn btn-green">
-            Open account
-          </a>
-        </div>
-      </section>
+      <GetStarted/>
     </div>
   );
 }
